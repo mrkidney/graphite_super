6fdc655b8ef053f72bc69744565c65e335da6c07
mrkidney <azweig@stanford.edu>
2017-10-18 14:25:50 -0700

    threshold?

--- gae/gae/train.py
+++ gae/gae/train.py
@@ -36,7 +36,7 @@ flags.DEFINE_integer('auto_node', 0, '1 for autoregressive by node')
 flags.DEFINE_integer('vae', 1, '1 for doing VGAE embeddings first')
 flags.DEFINE_integer('test', 1, 'Number of tests for mean and std')
 flags.DEFINE_float('auto_dropout', 0., 'Dropout for specifically autoregressive neurons')
-flags.DEFINE_float('threshold', 0.5, 'Threshold for autoregressive graph prediction')
+flags.DEFINE_float('threshold', 0.51, 'Threshold for autoregressive graph prediction')
 
 flags.DEFINE_integer('parallel', 1, 'Internal bullshit')
 flags.DEFINE_string('dataset', 'cora', 'Dataset string.')
