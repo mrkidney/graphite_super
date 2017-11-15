61babddd966205ffd760f4398fcbbbc3dd18c290
mrkidney <azweig@stanford.edu>
2017-11-14 16:54:17 -0800

    just semi

--- gae/gae/optimizer.py
+++ gae/gae/optimizer.py
@@ -26,13 +26,15 @@ class OptimizerSemi(object):
         preds_sub = preds
         labels_sub = labels
 
-        self.cost = norm * tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=preds_sub, targets=labels_sub, pos_weight=pos_weight))
+        self.cost = 0
+        if FLAGS.model == 'graphite':
+            self.cost = norm * tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=preds_sub, targets=labels_sub, pos_weight=pos_weight))
 
-        self.log_lik = self.cost
+            self.log_lik = self.cost
 
-        if FLAGS.vae:
-            self.kl = (0.5 / num_nodes) * tf.reduce_mean(tf.reduce_sum(1 + 2 * model.z_log_std - tf.square(model.z_mean) - tf.square(tf.exp(model.z_log_std)), 1))
-            self.cost -= self.kl
+            if FLAGS.vae:
+                self.kl = (0.5 / num_nodes) * tf.reduce_mean(tf.reduce_sum(1 + 2 * model.z_log_std - tf.square(model.z_mean) - tf.square(tf.exp(model.z_log_std)), 1))
+                self.cost -= self.kl
 
         self.cost += FLAGS.weight_decay * model.weight_norm
 
