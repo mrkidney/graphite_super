60082d65e5072e002a69fcbe9d54d8ec1140f4e2
mrkidney <azweig@stanford.edu>
2017-11-15 12:19:11 -0800

    just semi

--- gae/gae/model.py
+++ gae/gae/model.py
@@ -196,7 +196,7 @@ class GCNModelFeedback(Model):
                                       output_dim=FLAGS.hidden4,
                                       act=tf.nn.relu,
                                       adj = self.adj,
-                                      dropout=self.dropout/5.0,
+                                      dropout=0.,
                                       logging=self.logging)        
 
         output = GraphConvolution(input_dim=FLAGS.hidden4,
