68d37deb1f2285d450acf8460705c088d9083d82
mrkidney <azweig@stanford.edu>
2018-04-15 14:44:22 -0700

    ugh, hopefully

--- gae/gae/model.py
+++ gae/gae/model.py
@@ -231,7 +231,7 @@ class GCNModelFeedback(Model):
 
     def encoder_y(self, z1, inputs):
         hidden = self.hidden_y_layer_x(inputs) + self.hidden_y_layer_z1(z1)
-        #hidden = tf.nn.relu(hidden)
+        hidden = tf.nn.relu(hidden)
         return self.y_layer(hidden)
 
     def encoder_z2(self, z1, y):
