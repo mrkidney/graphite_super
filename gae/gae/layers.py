eb4d94c13d000232214e68bed71e76cdf790db9f
mrkidney <azweig@stanford.edu>
2018-04-15 13:42:26 -0700

    ugh, hopefully

--- gae/gae/layers.py
+++ gae/gae/layers.py
@@ -150,6 +150,7 @@ class FiveGraphAttention(Layer):
     def __init__(self, input_dim, output_dim, adj, features_nonzero, dropout=0., act=tf.nn.relu, **kwargs):
         super(FiveGraphAttention, self).__init__(**kwargs)
         with tf.variable_scope(self.name + '_vars'):
+            self.vars['weights'] = weight_variable_glorot(input_dim, output_dim, name="weights")
             self.vars['l1'] = GraphAttention(input_dim, output_dim/5, adj, features_nonzero, dropout, act)
             self.vars['l2'] = GraphAttention(input_dim, output_dim/5, adj, features_nonzero, dropout, act)
             self.vars['l3'] = GraphAttention(input_dim, output_dim/5, adj, features_nonzero, dropout, act)
