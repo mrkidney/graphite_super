d234bff34136c0b24a9d406ab132e041efba0bbb
mrkidney <azweig@stanford.edu>
2018-04-16 09:22:53 -0700

    ugh, hopefully

--- gae/gae/model.py
+++ gae/gae/model.py
@@ -154,7 +154,7 @@ class GCNModelFeedback(Model):
                                           dropout=0.,
                                           logging=self.logging)
 
-        self.hidden_y_layer_x = FiveGraphAttention(input_dim=self.input_dim,
+        self.hidden_y_layer_x = GraphConvolutionSparse(input_dim=self.input_dim,
                                               output_dim=FLAGS.hidden_y,
                                               adj=self.adj,
                                               features_nonzero=self.features_nonzero,
