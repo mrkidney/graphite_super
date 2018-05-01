68bc78bf3aa6542e07cb38ace85ac2b7c45d1641
mrkidney <azweig@stanford.edu>
2018-05-01 16:26:18 -0700

    correct adj dropout

--- gae/gae/layers.py
+++ gae/gae/layers.py
@@ -197,9 +197,9 @@ class GraphAttention(Layer):
         a1 = tf.matmul(x, self.vars['a1'])
         a2 = tf.matmul(x, self.vars['a2'])
         alpha = tf.nn.leaky_relu(a1 + tf.transpose(a2))
-        adj = tf.sparse_tensor_to_dense(self.adj, validate_indices = False)
+        adj = tf.ceil(tf.sparse_tensor_to_dense(self.adj, validate_indices = False))
         adj = tf.nn.dropout(adj, 1 - self.dropout)
-        bias = tf.exp(tf.ceil(adj) * -10e9) * -10e9
+        bias = tf.exp(adj * -10e9) * -10e9
         alpha = tf.nn.softmax(alpha + bias)
 
         #alpha = tf.nn.dropout(alpha, 1 - self.dropout)
