576fcf4986b1172a43aa21e03ca5b8a2c0914893
mrkidney <azweig@stanford.edu>
2018-05-13 13:36:59 -0700

    trying the sparseness attention

--- gae/gae/layers.py
+++ gae/gae/layers.py
@@ -203,7 +203,7 @@ class GraphAttention(Layer):
         alpha = tf.SparseTensor(alpha.indices, tf.nn.leaky_relu(alpha.values), alpha.dense_shape)
         alpha = tf.sparse_softmax(alpha)
 
-        alpha = dropout_sparse(alpha, 1-self.dropout, tf.cast(tf.sparse_reduce_sum(adj), tf.int32))
+        alpha = dropout_sparse(alpha, 1-self.dropout, tf.cast(tf.reduce_sum(adj.values), tf.int32))
         x = tf.sparse_tensor_dense_matmul(alpha, x)
 
         # alpha = tf.nn.leaky_relu(a1 + tf.transpose(a2))
