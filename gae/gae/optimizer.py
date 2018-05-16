addbdd27f5d9199e92fe587dc03d33ad074dd74d
mrkidney <azweig@stanford.edu>
2018-05-16 10:53:38 -0700

    maximum protections against nan

--- gae/gae/optimizer.py
+++ gae/gae/optimizer.py
@@ -101,7 +101,7 @@ class OptimizerSemiGen(object):
 
         self.cost += (1.0 / num_nodes) * tf.reduce_mean(kl_categorical(y_semi, model.output_dim, model.labels_mask))
 
-        self.cost += (1.0 / num_nodes) * tf.reduce_mean(log_normal_pdf_tf(model.z1q_mean, model.z1q_log_std, model.z1q))
+        self.cost += (1.0 / num_nodes) * tf.reduce_mean(tf.maximum(log_normal_pdf_tf(model.z1q_mean, model.z1q_log_std, model.z1q), -10000))
 
         for label in range(model.output_dim):
             y_pos = tf.one_hot(indices = label, depth = model.output_dim)
@@ -112,7 +112,7 @@ class OptimizerSemiGen(object):
             z1p_mean, z1p_log_std = model.decoder_z1(z2, y_pos)
 
             self.cost -= (1.0 / num_nodes) * tf.reduce_mean(y_semi[:,label] * kl(z2_mean, z2_log_std))
-            self.cost -= (1.0 / num_nodes) * tf.reduce_mean(y_semi[:,label] * tf.maximum(log_normal_pdf_tf(z1p_mean, z1p_log_std, model.z1q), -100))
+            self.cost -= (1.0 / num_nodes) * tf.reduce_mean(y_semi[:,label] * tf.maximum(log_normal_pdf_tf(z1p_mean, z1p_log_std, model.z1q), -10000))
         
         self.cost *= FLAGS.tau
 
